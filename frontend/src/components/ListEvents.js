5b0f69bbceb215342f91855821a4b06c4b061540
Juan Espitia <js.espitia@uniandes.edu.co>
2020-08-20 12:04:07 -0500

    Frontend features

---
+++ frontend/src/components/ListEvents.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
+import Table from 'react-bootstrap/Table';
+import {Button} from 'react-bootstrap';
+import axios from './helpers/axios-requests';
+
+class ListEvents extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            requested : false,
+            eventos : [] 
+        }
+    }
+
+    componentDidMount(){
+        axios.get('/api/events', {headers: {'Authorization': `Token ${this.props.token}`}})
+        .then(res=>{
+            this.setState({
+                eventos: res.data,
+                requested:true
+            }
+        )})
+        .catch(e=>{
+            alert('Hubo un problema en la conexión con el servidor')
+        })
+    }
+    render(){
+        return <div>
+            <Link to='/event/create'>Crear un evento</Link>
+        {this.state.eventos && this.state.eventos.length?
+            <Table className="alternatebg">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Categoría</th>
+                        <th>Lugar del evento</th>
+                        <th>Dirección del Evento</th>
+                        <th>Desde</th>
+                        <th>Hasta</th>
+                        <th>Tipo</th>
+                        <th>Detalles</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.eventos.map((evt)=>{
+                        return <tr>
+                            <td>{evt.event_name}</td>
+                            <td>{evt.event_category}</td>
+                            <td>{evt.event_place}</td>
+                            <td>{evt.event_address}</td>
+                            <td>{evt.event_initial_date}</td>
+                            <td>{evt.event_final_date}</td>
+                            <td>{evt.event_type}</td>
+                            <td><Link to={`/events/${evt.id}`}>Ver evento</Link></td>
+                            <td><Button onClick={this.handleDelete(evt.id)}></Button></td>
+                        </tr>
+                    })}
+                </tbody>
+            </Table>
+            : this.state.requested? <p>No has creado eventos</p>: <p>Cargando</p> 
+        }
+        </div>
+    }
+    handleDelete=(id)=>{
+        axios.delete(`/api/event/${id}`, {headers: {'Authorization': `Token ${this.props.token}`}})
+        .then(res=>{
+            alert('Se ha eliminado el evento exitosamente')
+            window.location.reload();
+        })
+        .catch(e=>alert('No se ha podido eliminar el evento'))
+    }
+}
+
+const mapStatetoProps = state =>{
+    return {
+        token: state.token,
+    }
+}
+
+export default connect(mapStatetoProps)(ListEvents);
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {Redirect, Link} from 'react-router-dom'
+import axios from './helpers/axios-requests'
+var qs = require('qs');
+
+class Login extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {username: '',contrasena:'', values:{}};
+    }1
+    handleChangeCorreo = (event) => {
+        this.setState({correo: event.target.value});
+    }
+
+    handleChangeContrasena= (event) => {
+        this.setState({contrasena: event.target.value});
+    }
+    handleSubmit = async(event)=>{
+        let data = qs.stringify({
+            'username': this.state.username,
+            'password': this.state.contrasena 
+        });
+        await axios.post('/api/api-auth', data)
+        .then(res=>{
+            if(res.status >=200 && res.status <300){
+                if(res.data.token) this.props.onLoginSuccesfull(res.data.token)
+                else alert('Correo o contraseña incorrecto')
+            }
+            else alert('Correo o contraseña incorrecto')
+        })
+        .catch(e=>{
+            alert('Hubo un error en la autenticación')
+        })
+      }
+    render(){
+        if (this.props.authenticate ){
+            return <Redirect to="/"/>
+        }
+        return <div>
+        <div className="logoLogin">
+            <img src='/Escudo.svg' width={'350px'}/>
+        </div>
+        <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Nombre de usuario</Form.Label>
+                <Form.Control type="text" placeholder="Username" onChange={this.handleChangeCorreo} />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control type="password" placeholder="Password" onChange={this.handleChangeContrasena} />
+            </Form.Group>
+            <button className="yandw" type="submit">
+                Ingresar
+            </button>
+        </Form>
+        <div>
+            ¿No tienes cuenta? <br/><Link to='/register'>Crear usuario</Link>
+        </div>
+    </div>
+    }
+}
+const mapStatetoProps = state =>{
+    return {
+        authenticate: state.authenticate,
+        token: state.token,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        onLoginSuccesfull: (token)=>{
+            return dispatch({type:AUTHENTICATE, token:token})
+        } 
+    }
+}
+
+export default connect(mapStatetoProps,mapDispatchToProps)(Login);
